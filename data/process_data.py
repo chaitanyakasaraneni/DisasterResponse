1a3606a295376f941f04cc45dfee31d5d9c7ee5a
chaitanya_kasaraneni <kc.kasaraneni@gmail.com>
2020-04-27 22:45:11 +0000

    feat: ETL process to load, clean and store data from .csv to Database
    The file 'data/process_data.py' contains the functions to load data
    from a CSV file, apply cleaning techniques and store it back in a
    database using sqlite

--- data/process_data.py
+++ data/process_data.py
@@ -1,18 +1,76 @@
 import sys
-
+import pandas as pd
+import numpy as np
+from sqlalchemy.engine import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    '''
+    INPUT - messages csv file name and file path
+            categories csv file name and file path
+    OUTPUT - pandas dataframe 
+           1. read the message file into a pandas dataframe
+           2. read the categories file into a pandas dataframe
+           3. merge the messages dataframe and catergories dataframe
+           4. return the merged dataframe
+    '''
+
+    df_messages = pd.read_csv(messages_filepath)
+    
+    df_categories = pd.read_csv(categories_filepath)
+    
+    #merging messages and categories on 'id' column
+    df = pd.merge(df_messages, df_categories, on='id')
+    
+    return df
 
 
 def clean_data(df):
-    pass
+    '''
+    INPUT - pandas  dataframe
+    OUTPUT - pandas dataframe with cleaned data
+        1. create categories dataframe by spliting the categories column by ';'
+        2. rename the new columns created by splitting the categories with the category values.
+        3. Convert category values to just numbers 0 or 1.
+        4. merge the input dataframe and the message categories split columnn
+        5. remove any duplicate messages
+    '''
+
+    #creating a category for each category value which are found by seperating them
+    categories = df['categories'].str.split(';', expand=True)
+    
+    #creating category of columns
+    category_colnames = categories[:1].squeeze().apply(lambda x: x[:-2])
+
+    #replacing column names in the original dataframe
+    categories.columns = category_colnames
+
+    for column in categories:
+        
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda x: x[-1])
+    
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column], downcast="integer")
+
+    df.drop("categories", axis=1, inplace=True)
+    
+    #remove duplicates after merging with the categories table
+    df = df.join(categories).drop_duplicates()
+    
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
-
-
+    '''
+    INPUT - panda dataframe , database file name 
+    OUTPUT - pandas dataframe data stored to the database file 
+        1. Create a SQLlite database using SQLAlchamey packages
+        2. Load the input dataframe to the the SQL database.
+    '''
+    # Create database engine object
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('DisasterResponse', engine, index=False)
+   
 def main():
     if len(sys.argv) == 4:
 
@@ -29,7 +87,7 @@ def main():
         save_data(df, database_filepath)
         
         print('Cleaned data saved to database!')
-    
+   
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
