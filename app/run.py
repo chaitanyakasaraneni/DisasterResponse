d2d1248ec8d9de5218b79baa6593435525beefea
chaitanyakasaraneni <chaitanyakrishna755@gmail.com>
2020-12-11 20:13:56 -0800

    fix: fix error for joblib import

--- app/run.py
+++ app/run.py
@@ -8,7 +8,8 @@ from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+# from sklearn.externals import joblib
+import joblib
 from sqlalchemy import create_engine
 
 
